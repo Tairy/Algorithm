314382211d912e258ee37d5b181450272824aeb0
Tairy <tairyguo@gmail.com>
2016-04-25 10:58:21 +0800

    leet code !

---
+++ leetcode/two-sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSumAlph(self, nums, target):
+        for i in range(0, len(nums)):
+            for j in range(i, len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i, j]
+
+    def twoSumBeta(self, nums, target):
+        numMap = {}
+        for i in range(0, len(nums)):
+            numMap[nums[i]] = i
+
+        # print numMap
+        for key in numMap:
+            if(numMap.get(target - key)):
+                return [numMap[key], numMap[target - key]]
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.twoSumBeta([0,4,3,0], 0)
